51e57072bfde8022ae0022dafd68608438391fcb
gastonrepos <grmercau@gmail.com>
2021-09-12 15:38:11 -0300

    feat: :zap: [2] add component

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TypesOfComponents from './src/screens/TypesOfComponents';
+import ContadorClass from './src/screens/ContadorClass';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <TypesOfComponents/>
+      {/* <TypesOfComponents/> */}
+      <ContadorClass/>
     </View>
   );
 }
