187a11f17073d307b39f31885450a6faff3cd71b
gastonrepos <grmercau@gmail.com>
2021-09-16 12:07:32 -0300

    feat: :art: [1] add type render prop

--- src/screens/TypesOfComponents.js
+++ src/screens/TypesOfComponents.js
@@ -4,15 +4,24 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import Box from '../components/StatelessComponent';
 import ContainerBox from '../components/StateComponent';
 import Hoc from '../components/Hoc';
+import RenderProp from '../components/RenderProp';
 
 
-const Box2 = (data) => {
+const Box2 = (data) => {    
     return(
         <View style={ {backgroundColor: 'red'} }>
-            <Text>{data.data}</Text>
+            <Text style={ {fontSize: 25 } }>{data.data}</Text>
         </View>
     );
 }
+
+const Box3 = (data) => {
+    return(
+        <View style={ {backgroundColor: 'blue'} }>
+            <Text> Componente Render Prop { data } </Text>
+        </View>
+    )
+};
 
 const componenteHocBox1 = Hoc(Box);
 const componenteHocBox2 = Hoc(Box2);
@@ -21,10 +30,19 @@ const TypesOfComponents = () => {
     return (
         <View style= { styles.center }> 
             <Text> describe los distintos tipos de componentes que pueden crearse </Text>
+
             <Box data={'este es un componente sin estado'}/>
+
             <ContainerBox/>
+
+            { Hoc(Box) }
             { componenteHocBox1 }
+
+            { Hoc(Box2) }
             { componenteHocBox2 }
+
+            { RenderProp((data) =>  Box3(data)) }
+
             <Button
                 title='boton'
                 onPress={ () => {} }
