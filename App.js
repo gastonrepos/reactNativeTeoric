08e6c9643f4bc206b62c493384aca9a6ed578127
gastonrepos <grmercau@gmail.com>
2021-09-19 17:07:55 -0300

    feat: :art: [4] add Conditional Rendering component

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import TypesOfComponents from './src/screens/TypesOfComponents';
+import ConditionalRendering from './src/screens/ConditionalRendering';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <TypesOfComponents/>
+      <ConditionalRendering/>
     </View>
   );
 }
