8016e9c9e2ef9ecfa5c7e14d19c88b4c2dfd7501
gastonrepos <grmercau@gmail.com>
2021-09-22 09:39:52 -0300

    feat: :art: [5] add component list

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TypesOfComponents from './src/screens/TypesOfComponents';
+import ListWithKeys from './src/components/ListWithkeys';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <TypesOfComponents/>
+      {/* <TypesOfComponents/> */}
+      <ListWithKeys/>
     </View>
   );
 }
